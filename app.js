98902a448ca8b608bb0a4005de353b1e7f95d153
Pete Fiuk <piotrek.fiuk@gmail.com>
2023-08-29 15:34:06 +0200

    hide/show button added using v-if and v-show (div visibility)

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const app = Vue.createApp({
   data() {
     return {
+      showBooks: true,
       title: 'The Final Empire',
       author: 'Brandon Sanderson',
       age: 45
     }
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    }
+    }
   }
-});
+);
 
 app.mount('#app');
